3eb8041ab6757278c279bc980f9059f5494099be
zuiyuki-ctrl <wo20050526@qq.com>
2025-12-04 05:01:45 +0000

    Update

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,5 @@
 import rclpy     
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-# from person_msgs.srv import Query #使う型を変更
-# rclpy.init()
-# node = Node("talker")
-
-# def cb(request, response):
-#     if request.name == "Tom":
-#         response.age = 46
-#     else:
-#         response.age = 255
-
-#     return response
-
-
-# def main():
-#     srv = node.create_service(Query, "query", cb) #サービスの作成                     
-#     rclpy.spin(node)
-
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 rclpy.init() 
