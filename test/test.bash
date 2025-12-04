62472beca8675ed95282d4ae2f3ebc8d9768bc0b
zuiyuki-ctrl <wo20050526@qq.com>
2025-12-04 05:03:44 +0000

    Update

--- test/test.bash
+++ test/test.bash
@@ -8,4 +8,5 @@ colcon build
 source $dir/.bashrc 
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | grep 'Listen: 10'
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
